01e6988227bbc88c6fbe925c1824f5b1cbf18164
rhkdgns95 <rhkdgns9489@gmail.com>
2020-06-25 23:43:04 +0900

    Login Page (#4)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ConfigProvider } from "antd";
+import ko from "antd/es/locale/ko_KR";
 import App from "./App";
 import "./index.css";
+import { GlobalStyles } from "./styles/global-styles";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ConfigProvider locale={ko}>
+    <App />
+    <GlobalStyles />
+  </ConfigProvider>,
+  document.getElementById("root")
+);
